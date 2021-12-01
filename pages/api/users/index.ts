2a8b98f8b0313a08dfbe560683164ba603f25f24
Jorge0611 <49691994+Jorge0611@users.noreply.github.com>
2021-12-01 18:26:59 -0400

    Refractor

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,25 +1,27 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-
-  await fetch('https://api.clerk.dev/v1/users',{
+  await fetch("https://api.clerk.dev/v1/users", {
     method: "GET",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.NEXT_PRIVATE_CLERK_BACKEND_TOKEN}`
+      Authorization: `Bearer ${process.env.NEXT_PRIVATE_CLERK_BACKEND_TOKEN}`,
     },
-  }).then((response: Response) => {
-    if(response.ok){
-      return response.json();
-    }
-    throw  response;
-  }).then((data) => {
-    res.status(200).json({ users: data });
-  }).catch((error) => {
-    console.error("Error fetching data: " + error);
   })
+    .then((response: Response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw response;
+    })
+    .then((data) => {
+      res.status(200).json({ users: data });
+    })
+    .catch((error) => {
+      console.error("Error fetching data: " + error);
+    });
 }
